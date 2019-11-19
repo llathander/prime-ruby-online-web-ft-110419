7da4d1ae349fbc7ce84b6d152fffa8ab1020c9fb
Adrian Samaniego <llathander@gmail.com>
2019-11-19 19:40:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+@number = [17, 3, 4, 5, 6, 1, 2, 8]
+@otherNumber = 10
+
+def prime?(x)
+  if x.each do |z|
+    z % 1 == 0
+  return true
+  #elsif  % 1 != 0
+  return false
+ end
+end
+end
+
+prime?(@number)
+#prime?(@otherNumber)
